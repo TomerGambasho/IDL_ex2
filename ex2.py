6c4f9b5c4151c2af127be9df4c57be3c0b6a3ebe
TomerGambasho <122287569+TomerGambasho@users.noreply.github.com>
2025-05-11 12:44:28 +0300

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# === Imports ===
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
